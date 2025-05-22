df79bd0f7b89ad5516d05711a38cb9c77124d5a7
RaghavAlways <ravendrakumara004@gmail.com>
2025-05-22 16:55:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,8 +39,6 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') 
-API_KEY = getenv("API_KEY", '30DxNexGenBots6fab95')
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/thanos_network")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/thanos_network")
